208876433645b961da4c4525b509a22de36e24d4
Enor Cerna <u17200479@utp.edu.pe>
2023-07-12 07:28:30 -0500

    add chrome

--- src/generator/video/LinkExtract.ts
+++ src/generator/video/LinkExtract.ts
@@ -1,6 +1,8 @@
 import puppeteer from "puppeteer"
+import chrome from "chrome-aws-lambda"
+
 async function LinkExtra(url: string) {
-  const browser = await puppeteer.launch( { headless: "new", args: [ "--no-sandbox" ] } )
+  const browser = await puppeteer.launch( { headless: "new", args: [ ...chrome.args, "--no-sandbox", '--hide-scrollbars', '--disable-web-security' ], defaultViewport: chrome.defaultViewport, executablePath: await chrome.executablePath, ignoreHTTPSErrors:true} )
   const page = await browser.newPage()
   await page.goto( url )
   const video = await page
