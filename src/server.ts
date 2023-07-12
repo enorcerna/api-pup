df0254ea5774ccba95ed88c7d2c62c6ac21c9b09
Enor Cerna <u17200479@utp.edu.pe>
2023-07-11 23:57:11 -0500

    serverless export

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { video } from './routes'
+import serverless from 'serverless-http'
 const app = express()
 
 app.use( '/api', video )
@@ -11,3 +12,5 @@ app.listen( 1007, () =>
 {
     console.log( 'Server started at http://localhost:1007' )
 })
+
+export const handler = serverless( app )
