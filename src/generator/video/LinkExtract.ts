0c7c87bb9d9570812c7c01bcb8ec32217ce408a6
Enor Cerna <u17200479@utp.edu.pe>
2023-07-12 08:05:22 -0500

    new confi

--- src/generator/video/LinkExtract.ts
+++ src/generator/video/LinkExtract.ts
@@ -2,7 +2,7 @@ import puppeteer from "puppeteer-core"
 import chrome from "chrome-aws-lambda"
 
 async function LinkExtra(url: string) {
-  const browser = await puppeteer.launch( { headless: "new", args: [ ...chrome.args, "--no-sandbox", '--hide-scrollbars', '--disable-web-security' ], defaultViewport: chrome.defaultViewport, executablePath: await chrome.executablePath, ignoreHTTPSErrors:true} )
+  const browser = await puppeteer.launch( { headless: true, args: [ ...chrome.args, "--no-sandbox", '--hide-scrollbars', '--disable-web-security' ], defaultViewport: chrome.defaultViewport, executablePath: await chrome.executablePath, ignoreHTTPSErrors:true ,channel:'chrome'} )
   const page = await browser.newPage()
   await page.goto( url )
   const video = await page
