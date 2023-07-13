064c1b7f4bab59f6d2406f088ad8064dec23c3cf
Enor Cerna <u17200479@utp.edu.pe>
2023-07-12 22:50:26 -0500

    add nigthmare & remove unused

--- src/generator/video/LinkExtract.ts
+++ src/generator/video/LinkExtract.ts
@@ -1,14 +1,11 @@
-import puppeteer from "puppeteer-core"
-import chrome from "chrome-aws-lambda"
 
-async function LinkExtra(url: string) {
-  const browser = await puppeteer.launch( { headless: true, args: [ ...chrome.args, "--no-sandbox", '--hide-scrollbars', '--disable-web-security' ], defaultViewport: chrome.defaultViewport, executablePath: await chrome.executablePath, ignoreHTTPSErrors:true ,channel:'chrome'} )
-  const page = await browser.newPage()
-  await page.goto( url )
-  const video = await page
-			.waitForSelector('video')
-			.then((el) => el?.getProperty('src').then((src) => src?.jsonValue()) as Promise<string>);
-  await browser.close()
+import Nigthmare from "nightmare"
+
+async function LinkExtra ( url: string )
+{
+  const browser = new Nigthmare( { show: false } )
+  const video = await browser.goto( url ).wait( 'video' ).evaluate( () => document.querySelector( 'video' )?.getAttribute( 'src' ) )
+  await browser.end()
   return video
 }
 export default LinkExtra
